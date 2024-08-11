7281be87c0670a819967697e63b84da445c7583c
ARAFAT <munshiarafathussain@gmail.com>
2024-08-11 15:04:33 +0600

    CORS policy updated

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({ 
-  origin: 'http://localhost:5173',
+  origin: 'https://blogbyarafat.vercel.app',
   credentials: true,
 }));
 
