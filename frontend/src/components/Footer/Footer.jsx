b9131c3fd04fcba97cd3f9a30d3034c4ca9d69d5
ARAFAT <munshiarafathussain@gmail.com>
2024-08-12 19:23:49 +0600

    error fixed

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logo from './logo.png'
+
 
 function Footer() {
   return (
-    <section className="relative overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
+    <section className="relative overflow-hidden py-10 bg-white border border-t-2 border-t-slate-200">
             <div className="relative z-10 mx-auto max-w-7xl px-4">
                 <div className="-m-6 flex flex-wrap">
                     <div className="w-full p-6 md:w-1/2 lg:w-5/12">
                         <div className="flex h-full flex-col justify-between">
                             <div className="mb-4 inline-flex items-center">
-                                <Logo width="100px" />
+                                <img src="/logo.png" alt="Logo" className='h-8' />
                             </div>
                             <div>
                                 <p className="text-sm text-gray-600">
